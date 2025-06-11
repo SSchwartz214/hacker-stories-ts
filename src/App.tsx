6c0fc6d6ca02e9cf909992ff2558fa1b922082e9
SethSchwartz <seth.schwartz@thoughtworks.com>
2025-06-11 16:47:59 -0400

    Lists in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-function getTitle(title: string) {
-  return title
-}
-
-const animals = ['tiger', 'bear', 'moose']
+const list = [
+  {
+  title: 'React',
+  url: 'https://react.dev/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
 
 function App() {
   return (
     <div>
-      <h1>
-        Hello {getTitle('React')}
-      </h1>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input type="text" id="search" />
+
+      <hr/>
 
       <ul>
-        {animals.map((animal) =>
-          <li>{animal}</li>
+        {list.map((item) =>
+            <li key={item.objectID}>{item.title}
+              <span><a href={item.url} /></span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+           </li>
         )}
       </ul>
-      <label htmlFor='search'>Search: </label>
-        <input type="text" id="search" />
     </div>
   )
 }
