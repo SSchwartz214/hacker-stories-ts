06661ffa942cec1ed2f78cc9ca7bd178c6ac8c9e
SethSchwartz <seth.schwartz@thoughtworks.com>
2025-06-20 15:27:44 -0400

    Props and State

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,35 @@
-type Item = {
+import * as React from "react"
+
+type Story = {
+  objectID: number
   title: string
   url: string
   author: string
   num_comments: number
   points: number
-  objectID: number
 }
 
-const list1 = [
-  {
-  title: 'React',
-  url: 'https://react.dev/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-];
 
-  const list2 = [
+const App = () => {
+  const stories = [
     {
-    title: 'Python',
-    url: 'https://python.dev/',
-    author: 'Bob Man',
-    num_comments: 4,
-    points: 5,
-    objectID: 2,
+    title: 'React',
+    url: 'https://react.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
     },
     {
-    title: 'Ruby',
-    url: 'https://ruby.org/',
-    author: 'DHH',
-    num_comments: 7,
-    points: 6,
-    objectID: 3,
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
     },
   ];
 
-const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -54,58 +38,60 @@ const App = () => {
 
       <hr/>
 
-      <List list={list1} />
-      <List list={list2} />
+      <List list={stories} />
     </div>
   )
 }
 
-type ItemProps = {
-  item: {
-    title: string
-    url: string
-    author: string
-    num_comments: number
-    points: number
-    objectID: number
-  }
-}
-const Item = ({item}: ItemProps) => (
-  <li>
-    <span>{item.title}</span>
-    <span><a href={item.url} /></span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </li>
-)
-
-type ListProps = {
-  list: Item[]
-}
-
-const List = ({list}: ListProps) => (
-    <ul>
-      {list.map((item) => (
-        <Item key={item.objectID} item={item}/>
-      )
-      )}
-    </ul>
-)
-
-
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('')
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event)
-    console.log(event.target.value)
+    setSearchTerm(event.target.value)
   }
 
  return (
   <div>
     <label htmlFor='search'>Search: </label>
     <input type="text" id="search" onChange={handleChange} />
+
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+    </p>
   </div>
  )
+}
+
+type ListProps = {
+  list: Story[]
+}
+
+const List = ({list}: ListProps) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item}/>
+      )
+      )}
+    </ul>
+  )
+}
+
+type ItemProps = {
+  item: Story
+}
+
+const Item = ({item}: ItemProps) => {
+  return (
+    <li>
+      <span>
+        <a href={item.url}> {item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  )
 }
 
 export default App
