be1e0ef0c68a26e5caaaac06630e90fa3ba3c952
SethSchwartz <seth.schwartz@thoughtworks.com>
2025-06-23 18:50:47 -0400

    Callback handlers and lifting state

--- src/App.tsx
+++ src/App.tsx
@@ -30,34 +30,35 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('')
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const searchedStories =  stories.filter((story) => (story.title.toLowerCase().includes(searchTerm.toLowerCase())))
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search/>
+      <Search onSearch={handleSearch}/>
 
       <hr/>
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   )
 }
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = React.useState('')
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value)
-  }
-
+type SearchProps = {
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+const Search = ({onSearch}: SearchProps) => {
  return (
   <div>
     <label htmlFor='search'>Search: </label>
-    <input type="text" id="search" onChange={handleChange} />
-
-    <p>
-      Searching for <strong>{searchTerm}</strong>
-    </p>
+    <input type="text" id="search" onChange={onSearch} />
   </div>
  )
 }
